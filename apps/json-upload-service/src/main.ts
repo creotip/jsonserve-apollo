03b4240a8a0106c6c2312762f292e81dd18bed0d
ruslanelishaev <ruslan@byondxr.com>
2022-08-16 10:25:44 +0300

    rate limiter

--- apps/json-upload-service/src/main.ts
+++ apps/json-upload-service/src/main.ts
@@ -12,7 +12,7 @@ const mongodbURI = process.env.MONGODB_URI
 const dbName = process.env.MONGODB_NAME
 
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
+	windowMs: 5 * 60 * 1000, // 15 minutes
 	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
