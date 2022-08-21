e977a3bf5e03ac3262696600f69211827c6887ef
ruslanelishaev <ruslan@byondxr.com>
2022-08-21 17:30:31 +0300

    document file

--- apps/jsonserve-app/config/theme.ts
+++ apps/jsonserve-app/config/theme.ts
@@ -1,8 +1,7 @@
 import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
-	fonts: {
-		// heading: `'Barlow', sans-serif`,
-		// body: `'Barlow', sans-serif`,
+	config: {
+		initialColorMode: 'light',
 	},
 })
